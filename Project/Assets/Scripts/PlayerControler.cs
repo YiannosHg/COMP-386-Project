9528f61e1a25c515e8e9abc018d83593ed3147bc
YiannosHg <48808892+YiannosHg@users.noreply.github.com>
2021-12-10 20:40:23 +0200

    Added losing condition and sounds

--- Project/Assets/Scripts/PlayerControler.cs
+++ Project/Assets/Scripts/PlayerControler.cs
@@ -10,6 +10,12 @@ public class PlayerControler : MonoBehaviour
     public Text countText; // The text (count) to be shown 
     public Text winText; // The text to be shown when game finishes
 
+    // Sounds
+    public AudioClip collectPickUp;
+    public AudioClip collectDoNotPickUp;
+    private AudioSource sound;
+
+    // Movemnet
     private float xInput;
     private float zInput;
 
@@ -21,6 +27,7 @@ public class PlayerControler : MonoBehaviour
         count = 0; // Initialize counter
         countText.text = "Count: " + count.ToString(); // Show count
         winText.text = ""; // Set winText to empty string
+        sound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -48,11 +55,14 @@ public class PlayerControler : MonoBehaviour
         rb.AddForce(new Vector3(xInput, 0.0f, zInput) * moveSpeed);
     }
 
-    // Collision detection to deactivate the PickUp object we touched
+    // Collision detection to deactivate the PickUp object we touched and make sound
+    // and lose the game if we touched a DoNotPickUp object and make sound
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("PickUp"))
         {
+            sound.clip = collectPickUp; // Set sound value
+            sound.Play(); // Play sound
             other.gameObject.SetActive(false);
             ++count;
             countText.text = "Count: " + count.ToString();
@@ -61,6 +71,15 @@ public class PlayerControler : MonoBehaviour
             {
                 winText.text = "You Win!";
             }
+        }
+        else if(other.gameObject.CompareTag("DoNotPickUp"))
+        {
+            sound.clip = collectDoNotPickUp; // Set sound value
+            sound.Play(); // Play sound
+            other.gameObject.SetActive(false);
+            count = -1; // Prevents the player from winning the game
+            winText.text = "You Lose!";
+            countText.enabled = false;
         }
     }
 }
